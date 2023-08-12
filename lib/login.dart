17bc0279a6ed19c4bfef3a6ff2b914400c429f69
Parthiban <m.parthiban7845@gmail.com>
2023-08-12 23:27:41 +0530

    Front page design done

--- lib/login.dart
+++ lib/login.dart
@@ -1,46 +1,117 @@
 import 'package:flutter/material.dart';
 
-
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Color.fromARGB(255, 92, 75, 81),
       body: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          SizedBox(height: 150),
           Image.asset("assets/images/logo.png"),
           SizedBox(height: 20),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Username",
-              hintStyle: TextStyle(color: Colors.grey),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(100)),
-              filled: true,
-              fillColor: Colors.white.withOpacity(0.15)
+          SizedBox(
+            width: 300,
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Username",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  //border: OutlineInputBorder(
+                  // borderRadius: BorderRadius.circular(100)),
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.15)),
             ),
-          
           ),
           SizedBox(height: 20),
-          TextField(
-              decoration: InputDecoration(
-                  hintText: "Password",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border:OutlineInputBorder(borderRadius: BorderRadius.circular(100)),
-                  filled: true,
-                  fillColor: Colors.white.withOpacity(0.15)
+          SizedBox(
+            width: 300,
+            child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Password",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    //border: OutlineInputBorder(
+                    // borderRadius: BorderRadius.circular(100)),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.15))),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: TextButton(
+                onPressed: () {
+                  print('Hello Bro!');
+                },
+                child: Text("Forgotten Password?")),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("Log in")),
+          SizedBox(height: 30),
+          Text(
+            "or sign in using",
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: 300,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    "assets/images/google.png",
+                    height: 35,
+                    width: 35,
+                  ),
+                  SizedBox(width: 30),
+                  Text(
+                    "Login using Google",
+                    style: TextStyle(color: Colors.black),
                   )
+                ],
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100)))),
             ),
-                  
-          TextButton(
-              onPressed: () {
-                print('Hello Bro!');
-              },
-              child: Text("Forget Password")),
-          ElevatedButton(onPressed: () {}, child: Text("Log in"))
+          ),
+          SizedBox(height: 15),
+          SizedBox(
+            width: 300,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    "assets/images/facebook.png",
+                    height: 35,
+                    width: 35,
+                    //color Color.fromARGB(255, 230, 2, 2).withOpacity(0),
+                  ),
+                  SizedBox(width: 30),
+                  Text(
+                    "Login using Facebook",
+                    style: TextStyle(color: Colors.black),
+                  )
+                ],
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100)))),
+            ),
+          )
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+        theme:ThemeData(fontFamily: "WorkSans"),
         home: LoginPage()
             );
   }
