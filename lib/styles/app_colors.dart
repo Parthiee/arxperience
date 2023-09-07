b1e5d2083e4c9591ac13fd2a4fc36a7d4bf8b92f
Parthiee <100670393+Parthiee@users.noreply.github.com>
2023-09-07 21:08:54 +0530

    Delete lib directory

--- lib/styles/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color(0xff344955);
-  static const primary1 = Color(0xff232f34);
-  static const primary2 = Color(0xff4a6572);
-  static const secondary = Color(0xfff9aa33);
-  static const textfield_color = Colors.grey;
-}
