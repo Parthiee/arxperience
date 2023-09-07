a68de8f04b022680222e2d1ab1450532f0691aaa
Parthiee <100670393+Parthiee@users.noreply.github.com>
2023-09-07 21:16:20 +0530

    Add files via upload

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff7c9a92);
+  static const primary1 = Color(0xff253334);
+  static const primary2 = Color(0xff4a6572);
+  static const secondary = Color(0xfff9aa33);
+  static const textfield_color = Colors.grey;
+}
